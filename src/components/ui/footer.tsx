17dc462bd0253862f0573caf712d908204542ddd
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2025-05-22 10:22:25 -0500

    changed navbar and footer border colors

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="w-full text-center text-xs text-muted-foreground mt-12 py-6 border-t">
+    <footer className="w-full text-center text-xs text-muted-foreground mt-12 py-6 border-t border-muted-foreground">
       &copy; {new Date().getFullYear()} Amir Abdur-Rahim. All rights reserved.
     </footer>
   );
