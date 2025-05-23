68da14149f30993419fde928afbc806242fd916c
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2025-05-23 00:50:08 -0500

    updated footer border to fully extend to edges of the screen

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="w-full text-center text-xs bg-background text-muted-foreground py-6 border-t border-muted-foreground">
+    <footer className="w-screen text-center text-xs bg-background text-muted-foreground py-6 border-t border-muted-foreground">
       &copy; {new Date().getFullYear()} Amir Abdur-Rahim. All rights reserved.
     </footer>
   );
